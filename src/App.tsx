cbe1a7f52cf2826aeefaa6a981e2a6893d13305c
Vahe Sevachyan <vscode13@gmail.com>
2023-12-29 21:49:29 -0800

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 // import "./App.css";
 
+import AddCategoryBtn from "./components/AddCategoryBtn";
+
 function App() {
   return (
-    <div className="m-container">
+    <div className="m-container bg-slate-300">
       <h1>Nexus Vault</h1>
       <div className="category-container">
         <span>Link Manager</span>
+        <AddCategoryBtn />
       </div>
     </div>
   );
---
+++ src/components/AddCategoryBtn.tsx
@@ -0,0 +1,12 @@
+import { FaPlus } from "react-icons/fa6";
+import { PlusIcon } from "./PlusIcon";
+const AddCategoryBtn = () => {
+  return (
+    <button>
+      {/* <FaPlus /> */}
+      <PlusIcon></PlusIcon>
+    </button>
+  );
+};
+
+export default AddCategoryBtn;
