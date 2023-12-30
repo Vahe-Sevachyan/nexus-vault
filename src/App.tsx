4de9a57908bb8b8876b662d33153afbc7bd555a8
Vahe Sevachyan <vscode13@gmail.com>
2023-12-29 21:25:11 -0800

    updated component folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="m-container">
       <h1>Nexus Vault</h1>
-    </>
+      <div className="category-container">
+        <span>Link Manager</span>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/PlusIcon.tsx
@@ -0,0 +1,3 @@
+export const PlusIcon = () => {
+  return <div>PlusIcon</div>;
+};
